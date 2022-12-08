286fcb6fd635cf615b0540fe3b5ad28d2ca8f1de
Failj Sagdiev <tczero.70@gmail.com>
2022-12-08 20:18:49 +0300

    Completed some kata

---
+++ JS/#3Strings.js
@@ -0,0 +1,10 @@
+let [a1,a2,b1,b2,c1,c2,d1,d2,e1,e2,n1,n2] = ["A","a","B","b","C","c","D","d","E","e","N","n"];
+var Dad = () => `${d1}${a2}${d2}`
+var Bee = () => `${b1}${e2}${e2}`
+var banana = () => `${b2}${a2}${n2}${a2}${n2}${a2}`
+
+console.log(banana());
+
+let answer1 = () => 'no'
+let answer2 = () => 'no'
+let answer3 = () => 'yes'
